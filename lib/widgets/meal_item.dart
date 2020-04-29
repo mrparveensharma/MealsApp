64865096061fa2c64781a24f4cc7f70672c0ea35
Parveen Kumar <cu.16bcs1721@gmail.com>
2020-04-29 16:28:51 +0530

    Meals_Card

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -16,6 +16,28 @@ class MealItem extends StatelessWidget {
     @required this.duration,
   });
 
+  String get complexityTest {
+    if (complexity == Complexity.Simple) {
+      return "Easy";
+    } else if (complexity == Complexity.Hard) {
+      return "Difficult";
+    } else {
+      return "Very Hard";
+    }
+  }
+
+  String get affordabilityTest {
+    if (affordability == Affordability.Affordable) {
+      return "Affordable";
+    } else if (affordability == Affordability.Pricey) {
+      return "Pricey";
+    } else if (affordability == Affordability.Luxurious) {
+      return "Luxurious";
+    } else {
+      return "Expensive";
+    }
+  }
+
   selectMeal() {}
   @override
   Widget build(BuildContext context) {
@@ -43,7 +65,60 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 260,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.work),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(complexityTest),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(affordabilityTest),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
